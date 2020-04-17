6750675c10c44cb2b4d2e6fffde6d8928d67c90f
Tateishi Tadatoshi <ishio39@gmail.com>
2020-04-18 01:22:33 +0900

    ADD: range_set.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use sudoku_rust::range_set::RangeSet;
+
 fn main() {
+    let a: bool = true;
+    let b = RangeSet::new();
+
+    println!("{}", a);
     println!("Hello, world!");
+    println!("{}", b);
 }
---
+++ src/range_set.rs
@@ -0,0 +1,28 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct RangeSet {
+    value: Vec<bool>,
+}
+
+impl RangeSet {
+    pub fn new() -> RangeSet {
+        RangeSet { value: (1..=9).into_iter().map(|_| true).collect() }
+    }
+}
+
+impl fmt::Display for RangeSet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = self.value
+            .iter()
+            .enumerate()
+            .map(|(n, b)|
+                 if *b {
+                     (n+1).to_string()
+                 } else {
+                     String::from("")
+                 })
+            .collect::<String>();
+        write!(f, "[{}]", str)
+    }
+}
