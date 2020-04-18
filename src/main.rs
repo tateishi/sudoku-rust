003ce9d9845a278b2c6462f25acce290aa501a22
Tateishi Tadatoshi <tateishi@tw.tokai-tv.co.jp>
2020-04-18 13:51:54 +0900

    ADD: clear(), union(), intersection()

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,18 @@ use sudoku_rust::range_set::RangeSet;
 fn main() {
     let a: bool = true;
     let b = RangeSet::new();
+    let c = RangeSet::clear();
 
     println!("{}", a);
     println!("Hello, world!");
     println!("{}", b);
+    let b = b.reset(3);
+    println!("{}", b);
+
+    println!("{}", c);
+    let c = c.set(3);
+    println!("{}", c);
+
+    println!("{}", b.union(&c));
+    println!("{}", b.intersection(&c));
 }
--- src/range_set.rs
+++ src/range_set.rs
@@ -9,6 +9,42 @@ impl RangeSet {
     pub fn new() -> RangeSet {
         RangeSet { value: (1..=9).into_iter().map(|_| true).collect() }
     }
+
+    pub fn clear() -> RangeSet {
+        RangeSet { value: (1..=9).into_iter().map(|_| false).collect() }
+    }
+
+    pub fn set(&self, idx: usize) -> RangeSet {
+        let v = self.value
+            .iter()
+            .enumerate()
+            .map(|(n, &b)| if n+1 == idx { true } else { b })
+            .collect();
+        RangeSet { value: v }
+    }
+
+    pub fn reset(&self, idx: usize) -> RangeSet {
+        let v = self.value
+            .iter()
+            .enumerate()
+            .map(|(n, &b)| if n+1 == idx { false } else { b })
+            .collect();
+        RangeSet { value: v }
+    }
+
+    pub fn union(&self, other: &RangeSet) -> RangeSet {
+        let v = self.value.iter().zip(other.value.iter())
+            .map(|(&a, &b)| if a || b { true } else { false })
+            .collect();
+        RangeSet { value: v }
+    }
+
+    pub fn intersection(&self, other: &RangeSet) -> RangeSet {
+        let v = self.value.iter().zip(other.value.iter())
+            .map(|(&a, &b)| if a && b { true } else { false })
+            .collect();
+        RangeSet { value: v }
+    }
 }
 
 impl fmt::Display for RangeSet {
